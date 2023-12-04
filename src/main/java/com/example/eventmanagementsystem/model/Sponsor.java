aa8f38428a769d839264d9c5f2d94a4b2bb153a4
SuryaPrakashDora <prakashmadakam21@gmail.com>
2023-12-04 03:28:10 +0000

    solution

---
+++ src/main/java/com/example/eventmanagementsystem/model/Sponsor.java
@@ -0,0 +1,71 @@
+/*
+ * You can use the following import statements
+ *
+ * import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+ * 
+ * import javax.persistence.*;
+ * import java.util.List;
+ * 
+ */
+
+// Write your code here
+package com.example.eventmanagementsystem.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "sponsor")
+public class Sponsor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int sponsorId;
+    @Column(name = "name")
+    private String sponsorName;
+    @Column(name = "industry")
+    private String Industry;
+
+    @ManyToMany
+    @JoinTable(name = "event_sponsor", joinColumns = @JoinColumn(name = "sponsorId"), inverseJoinColumns = @JoinColumn(name = "eventid"))
+
+    @JsonIgnoreProperties("sponsors")
+    private List<Event> events;
+
+    public Sponsor() {
+
+    }
+
+    public int getSponsorId() {
+        return sponsorId;
+    }
+
+    public void setSponsorId(int sponsorId) {
+        this.sponsorId = sponsorId;
+    }
+
+    public String getSponsorName() {
+        return sponsorName;
+    }
+
+    public void setSponsorName(String sponsorName) {
+        this.sponsorName = sponsorName;
+    }
+
+    public String getIndustry() {
+        return Industry;
+    }
+
+    public void setIndustry(String Industry) {
+        this.Industry = Industry;
+    }
+
+    public List<Event> getEvents() {
+        return events;
+    }
+
+    public void setEvents(List<Event> events) {
+        this.events = events;
+    }
+}
